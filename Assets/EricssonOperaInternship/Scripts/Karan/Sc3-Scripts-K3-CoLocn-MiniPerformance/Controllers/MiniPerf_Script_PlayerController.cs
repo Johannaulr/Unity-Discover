f02f9b99335c61c0b7e3c831bee7f8b7219d9562
Johanna <learning2make@gmail.com>
2023-08-27 19:26:18 +0200

    changes scene K3, BVC 18

--- Assets/EricssonOperaInternship/Scripts/Karan/Sc3-Scripts-K3-CoLocn-MiniPerformance/Controllers/MiniPerf_Script_PlayerController.cs
+++ Assets/EricssonOperaInternship/Scripts/Karan/Sc3-Scripts-K3-CoLocn-MiniPerformance/Controllers/MiniPerf_Script_PlayerController.cs
@@ -7,11 +7,18 @@ public class MiniPerf_Script_PlayerController : MonoBehaviour
     private bool audioPlayFlag;
     private bool passThruDarkFlag;
 
+    private bool profileUIVisibleFlag;
+
+    public GameObject ProfileMenuObject;
+    public GameObject ExperimentDescriptionObject;
+    public GameObject CameraObject;
+
     // Start is called before the first frame update
     void Start()
     {
         audioPlayFlag = false;
         passThruDarkFlag = false;
+        profileUIVisibleFlag = false;
     }
 
     // Update is called once per frame
@@ -43,6 +50,23 @@ public class MiniPerf_Script_PlayerController : MonoBehaviour
             {
                 MiniPerf_Script_SceneManager.instance.SendRPC(MiniPerfRPCtypes.NormalPassThru);
             }
+        }
+
+        if (OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger) > 0)
+        {
+            profileUIVisibleFlag = true;
+            ProfileMenuObject.SetActive(profileUIVisibleFlag);
+            ExperimentDescriptionObject.SetActive(profileUIVisibleFlag);
+            //ProfileMenuObject.transform.position = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch) + new Vector3(0.2f, 0, 0.15f);
+            ProfileMenuObject.transform.position = CameraObject.transform.position + new Vector3(0, -0.25f, .8f);
+            ExperimentDescriptionObject.transform.position = CameraObject.transform.position + new Vector3(0, 0.051f, 0.8f);
+
+        }
+        else if(OVRInput.Get(OVRInput.RawAxis1D.LHandTrigger) == 0)
+        {
+            profileUIVisibleFlag = false;
+            ProfileMenuObject.SetActive(profileUIVisibleFlag);
+            ExperimentDescriptionObject.SetActive(profileUIVisibleFlag);
         }
     }
 }
