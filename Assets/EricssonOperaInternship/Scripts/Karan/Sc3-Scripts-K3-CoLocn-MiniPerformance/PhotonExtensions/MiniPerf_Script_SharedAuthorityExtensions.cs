76357da6b6be1f0fa1979275e473dd7e4b53e6dd
Johanna <johannaulrichs@gmail.com>
2023-09-03 18:59:41 +0200

    AR 2 VR networked

--- Assets/EricssonOperaInternship/Scripts/Karan/Sc3-Scripts-K3-CoLocn-MiniPerformance/PhotonExtensions/MiniPerf_Script_SharedAuthorityExtensions.cs
+++ Assets/EricssonOperaInternship/Scripts/Karan/Sc3-Scripts-K3-CoLocn-MiniPerformance/PhotonExtensions/MiniPerf_Script_SharedAuthorityExtensions.cs
@@ -9,14 +9,15 @@ public static class MiniPerf_Script_SharedAuthorityExtensions
     * Relevant in shared topology only
     * TAKEN FROM:   https://doc.photonengine.com/fusion/current/technical-samples/fusion-vr-shared#grabbing_synchronization
     */
-    public static async Task<bool> WaitForStateAuthority(this NetworkObject o, float maxWaitTime = 8)
+    public static async Task<bool> WaitForStateAuthority(this NetworkObject netObject, float maxWaitTime = 8)
     {
-        float waitStartTime = Time.time;
-        o.RequestStateAuthority();
-        while (!o.HasStateAuthority && (Time.time - waitStartTime) < maxWaitTime)
-        {
-            await Task.Delay(1);
-        }
-        return o.HasStateAuthority;
+        return true;
+        //float waitStartTime = Time.time;
+        //netObject.RequestStateAuthority();
+        //while (!netObject.HasStateAuthority && (Time.time - waitStartTime) < maxWaitTime)
+        //{
+        //    await Task.Delay(1);
+        //}
+        //return netObject.HasStateAuthority;
     }
 }
